c50f73de963a08f6033c166b5941ade67e2a264a
unknown <mathapationkar8@gmail.com>
2021-09-12 12:15:16 +0530

    footer and other sections added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import react from 'react';
+
 import Navbar from './components/Navbar'
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home'
+import Services from './components/pages/Services'
+import Products from './components/pages/Products'
+import SignUp from './components/pages/SignUp'
 
 
 function App() {
@@ -12,6 +15,10 @@ function App() {
        <Navbar />
        <Switch>
          <Route path='/' exact component= {Home} />
+         <Route path='/services' exact component= {Services} />
+         <Route path='/products' exact component= {Products} />
+         <Route path='/sign-up' exact component= {SignUp} />
+         
          
 
          </Switch>
--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,51 +1,25 @@
 import React from 'react';
-import './Cards.css';
-import CardItem from './CardItem';
+import { Link } from 'react-router-dom';
 
-function Cards() {
+function CardItem(props) {
   return (
-    <div className='cards'>
-      <h1>Check out these EPIC Destinations!</h1>
-      <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
-            <CardItem
-              src='images/img-9.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
-              path='/services'
+    <>
+      <li className='cards__item'>
+        <Link className='cards__item__link' to={props.path}>
+          <figure className='cards__item__pic-wrap' data-category={props.label}>
+            <img
+              className='cards__item__img'
+              alt='Travel Image'
+              src={props.src}
             />
-            <CardItem
-              src='images/img-2.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
-              path='/services'
-            />
-          </ul>
-          <ul className='cards__items'>
-            <CardItem
-              src='images/img-3.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='Mystery'
-              path='/services'
-            />
-            <CardItem
-              src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
-              path='/products'
-            />
-            <CardItem
-              src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
-              path='/sign-up'
-            />
-          </ul>
-        </div>
-      </div>
-    </div>
+          </figure>
+          <div className='cards__item__info'>
+            <h5 className='cards__item__text'>{props.text}</h5>
+          </div>
+        </Link>
+      </li>
+    </>
   );
 }
 
-export default Cards;
+export default CardItem;
